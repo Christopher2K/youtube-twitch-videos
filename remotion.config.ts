6863f33db78c77fa354ebdd81002b537858917ac
Christopher N. KATOYI KABA <me@christopher2k.dev>
2021-11-10 21:52:55 -0500

    initial commit

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,14 @@
-import {Config} from 'remotion';
+import { Config } from 'remotion'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+Config.Rendering.setImageFormat('jpeg')
+Config.Output.setOverwriteOutput(true)
+Config.Bundling.overrideWebpackConfig((config) => {
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      plugins: [...(config.resolve?.plugins ?? []), new TsconfigPathsPlugin()],
+    },
+  }
+})
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,17 +1,17 @@
-import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
+import { Composition } from 'remotion'
+import { MyComposition } from 'Composition'
 
 export const RemotionVideo: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="Empty"
-				component={MyComposition}
-				durationInFrames={60}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
-		</>
-	);
-};
+  return (
+    <>
+      <Composition
+        id="Empty"
+        component={MyComposition}
+        durationInFrames={60}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+    </>
+  )
+}
